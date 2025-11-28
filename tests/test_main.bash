590edec105155a2a6cd6987f0fc76a2559d64595
Morofuji Junko <mimosa4kai@gmail.com>
2025-11-28 11:36:21 +0900

    Add test_main.bash

---
+++ tests/test_main.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2025 carpstreamer7
+# SPDX-License-Identifier: BSD-3-Clause
+
+res=0
+
+ng () {
+    echo "${1}行目が違います"
+    res=1
+}
+
+# famousquote が実行可能か
+out=$(echo "study" | ./main.py)
+[ "$?" -eq 0 ] || ng "$LINENO"
+
+# 出力が5語以内か（単語数チェック）
+word_count=$(echo "$out" | wc -w)
+[ "$word_count" -le 5 ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo "OK"
+exit $res
+
